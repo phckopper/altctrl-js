1cb70cc79f2a1bf0b8908d15c9592586e56d4dc4
Pedro Henrique Kopper <pedro.kopper@balena.io>
2022-06-09 07:53:13 -0300

    Correct this bindings

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var Emitter = require('events').EventEmitter;
 class Altctrl {
     constructor(url) {
         this.wsClient = new WebSocketClient();
+        this.eventEmitter = new Emitter();
         this.wsClient.connect(url);
         this.wsClient.on("connect", (connection) => {
-            connection.on("message", this._callback);
+            connection.on("message", this._callback.bind(this));
         });
-        this.eventEmitter = Emitter();
     }
 
     _callback(data) {
@@ -19,3 +19,5 @@ class Altctrl {
         return this.eventEmitter;
     }
 }
+
+module.exports = Altctrl
